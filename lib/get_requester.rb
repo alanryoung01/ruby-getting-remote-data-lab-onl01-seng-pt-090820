fe80ebcef43b691fcac266f49624992f7bbf12cd
Alan Young <alanryoung01@gmail.com>
2020-10-14 13:06:16 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,13 +4,14 @@ require 'json'
 
 
 class GetRequester
+  
   def initialize(url)
     @url = url
   end
 
   def get_response_body
     uri = URI.parse(@url)
-    response = Net:HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri)
     response.body
   end
 
